e7611db99dd8064f8644380b91b79d78ad6c92c5
kiwi334334 <willey.elias@explore.academy>
2023-05-23 17:45:54 +0000

    modified:   profile/src/App.svelte

--- profile/src/App.svelte
+++ profile/src/App.svelte
@@ -9,22 +9,32 @@
 	function start() {
 		document.getElementById("sign").style.display = "none";
 		document.getElementById("sign2").style.display = "none";
+		document.getElementById("hiddev").style.display = "block";
 	}
 </script>
+
 <Menubar />
 <main>
-	<input class="signin" id="sign" bind:value={name} placeholder="type name here" />
+	<input
+		class="signin"
+		id="sign"
+		bind:value={name}
+		placeholder="type name here"
+	/>
 	<button class="signin" id="sign2" on:click={signin}>Submit Name</button>
+	<div id="hiddiv" class="hid" />
 </main>
 
 <style>
-	.signin{
+	.signin {
 		margin-left: auto;
 	}
 	main {
-		margin-left: auto;
 		background-color: snow;
 		width: 100vw;
 		height: 100vh;
+	}
+	.hid {
+		display: none;
 	}
 </style>
